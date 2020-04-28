69c916b3265906956c8ae0e8a52a43365e46fd36
Thomas Mitchell <tfm@uoregon.edu>
2020-04-28 01:00:22 -0700

    Added AI to Chase-Down Player Units and Fixed Navigation OverBridge
    Enemy units now have an AI scrips that caused them to pursue the player's units, but will return after the player unit is destroyed or runs away. I also added a transparent plane over the bridge so that units can cross it more easily.

---
+++ Assets/Scripts/AIMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour
+{
+
+    public string MyEnemy;
+
+    Vector3 positionBeforePursuit;
+
+    bool inPursuit;
+
+    void Start()
+    {
+        inPursuit = false;
+        
+    }
+
+    void FixedUpdate()
+    {
+        GameObject[] units = GameObject.FindGameObjectsWithTag(MyEnemy);
+
+        foreach (GameObject g in units)
+        {
+            float distanceToEnemy = Vector3.Distance(g.transform.position, transform.position);
+
+            if (distanceToEnemy < 7 && !inPursuit)
+            {
+                inPursuit = true;
+                positionBeforePursuit = transform.position;
+                GetComponent<UnityEngine.AI.NavMeshAgent>().destination = g.transform.position;
+                return;
+            }
+            else if (distanceToEnemy < 7 && distanceToEnemy > 2 && inPursuit)
+            {
+                GetComponent<UnityEngine.AI.NavMeshAgent>().destination = g.transform.position;
+                return;
+            }
+        }
+
+        if (inPursuit)
+        {
+            inPursuit = false;
+            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = positionBeforePursuit;
+
+        }
+
+    }
+}
