0a062ba6cdb136ff8fa7e8ab007bf599e6352f8a
Thomas Mitchell <tfm@uoregon.edu>
2020-04-28 00:42:53 -0700

    Added Health and Attack Scripts
    All units now have health (out of 4). Units beside ResourceGatherers also have attack, which gradually damages nearby units belonging to the other player.

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public string MyEnemy;
+ 
+    void FixedUpdate()
+    {
+        GameObject[] units = GameObject.FindGameObjectsWithTag(MyEnemy);
+
+        foreach (GameObject g in units)
+        {
+            if (Vector3.Distance(g.transform.position, transform.position) < 3)
+            {
+                g.GetComponent<Health>().HealthLevel -= .015f;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float HealthLevel;
+
+    void Update()
+    {
+
+        TextMesh tm = GetComponentInChildren<TextMesh>();
+        tm.text = new string('-', Mathf.FloorToInt(HealthLevel));
+
+        Color color;
+
+        switch (Mathf.FloorToInt(HealthLevel))
+        {
+            default:
+                color = Color.green;
+                break;
+            case (3):
+                color = Color.magenta;
+                break;
+            case (2):
+                color = Color.yellow;
+                break;
+            case (1):
+                color = Color.red;
+                break;
+            case (0):
+                color = Color.white;
+                break;
+        }
+
+        tm.GetComponent<Renderer>().material.color = color;
+
+        tm.transform.forward = Camera.main.transform.forward;
+
+
+    }
+
+    void LateUpdate()
+    {  
+        if (HealthLevel < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
