791192fe73e79669f433dacc2a53aad5ca8b9992
Thomas Mitchell <tfm@uoregon.edu>
2020-04-29 21:16:31 -0700

    Factory Units Spawn in Front of Factory
    Factory units only spawn on one side of the factory now (positive x direction), instead of a random direction.

--- Assets/Scripts/FactoryBuild.cs
+++ Assets/Scripts/FactoryBuild.cs
@@ -18,9 +18,9 @@ public class FactoryBuild : MonoBehaviour
         {
             yield return new WaitForSeconds(11);
             Vector3 pos = transform.position;
-            float x = pos.x + Random.Range(-4.0f, 4.0f);
+            float x = pos.x + Random.Range(0f, 4.0f);
             float y = pos.y;
-            float z = pos.z + Random.Range(-4.0f, 4.0f);
+            float z = pos.z;
             float angle = Random.Range(0.0f, 360.0f);
             Instantiate(basicUnit, new Vector3(x, y, z), Quaternion.Euler(0.0f, angle, 0.0f));
         }
@@ -34,9 +34,9 @@ public class FactoryBuild : MonoBehaviour
         {
             yield return new WaitForSeconds(20);
             Vector3 pos = transform.position;
-            float x = pos.x + Random.Range(-4.0f, 4.0f);
+            float x = pos.x + Random.Range(0f, 4.0f);
             float y = pos.y;
-            float z = pos.z + Random.Range(-4.0f, 4.0f);
+            float z = pos.z;
             float angle = Random.Range(0.0f, 360.0f);
             Instantiate(advancedUnit, new Vector3(x, y, z), Quaternion.Euler(0.0f, angle, 0.0f));
         }
