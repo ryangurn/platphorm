8abe001fe4b143d786b8d60dabf7c792516cb1f1
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-01 18:45:32 -0700

    Adding Fonts + UI Scripts
    This is the start of a basic UI for the player. It will display different information. For now it helps display the player and enemy health, although it might not be super accurate.

---
+++ Assets/Scripts/UI/UIEnemyHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEnemyHealth : MonoBehaviour
+{
+	GameObject[] enemies;
+	Text health;
+	float total;
+
+	void Start()
+	{
+		health = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		int length = enemies.Length;
+		int i = 0;
+
+		float h = 0;
+		for (; i < length; i++)
+		{
+			if(enemies[i])
+			{
+				h += enemies[i].GetComponent<Health>().HealthLevel;
+			}
+		}
+
+		total = length * 4;
+
+		health.text = "Enemy Health: "+ h.ToString() + " / " + total.ToString();
+	}
+}
---
+++ Assets/Scripts/UI/UIPlayerHealth.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPlayerHealth : MonoBehaviour
+{
+	GameObject[] players;
+	Text health;
+	float total;
+
+	void Start()
+	{
+		health = GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		players = GameObject.FindGameObjectsWithTag("Enemy");
+		int length = players.Length;
+		int i = 0;
+
+		float h = 0;
+		for (; i < length; i++)
+		{
+			if(players[i])
+			{
+				h += players[i].GetComponent<Health>().HealthLevel;
+			}
+		}
+
+		total = length * 4;
+
+		health.text = "Player Health: "+ h.ToString() + " / " + total.ToString();
+	}
+}
