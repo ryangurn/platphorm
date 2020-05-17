3373374bf3744c8fdc56d77d45cc91657e0fdc86
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-17 08:41:57 -0700

    Adding A Simple Rotation Script
    This will rotate a game object about its y axis while the x axis is fixed to produce a nice rotation, this is mainly going to be used for the queue rendering.

---
+++ Assets/Scripts/UI/UIQueueRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIQueueRotation : MonoBehaviour
+{
+    Vector3 m_targetRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      m_targetRotation.y += 1;
+      transform.localRotation = Quaternion.Euler(m_targetRotation);
+    }
+}
