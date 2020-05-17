8d8439bb099d87d8b8f6a9d2e46063a6a2589b9a
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-17 09:16:17 -0700

    Cleaning Rotation Code

--- Assets/Scripts/UI/UIQueueRotation.cs
+++ Assets/Scripts/UI/UIQueueRotation.cs
@@ -6,12 +6,6 @@ public class UIQueueRotation : MonoBehaviour
 {
     Vector3 m_targetRotation;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
