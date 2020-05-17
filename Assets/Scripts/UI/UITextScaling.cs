c64022273639a936c3d837a74da62a5d005a3ccf
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-17 07:35:29 -0700

    Adding FontSize Scaler
    We are adding the font size scaler to help with changing the text sizing.

---
+++ Assets/Scripts/UI/UITextScaling.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextScaling : MonoBehaviour
+{
+    Text m_Text;
+
+    void Start()
+    {
+      m_Text = GetComponent<Text>();
+    }
+
+    public void Scale(Slider slide)
+    {
+      m_Text.fontSize = Mathf.RoundToInt(slide.value);
+    }
+}
