ebb2d5a7bdc7e96eddfa4e1111acbf2d50235cda
Thomas Mitchell <tfm@uoregon.edu>
2020-05-21 15:33:26 -0700

    AI now Comes in Waves
    When the AI forms a large group, it starts to move to the player platform and more aggressively spot units to fight. Also modified harvesters to circle around other objects, so they may not get stuck in WebGL anymore.

--- Assets/Scripts/AIMove.cs
+++ Assets/Scripts/AIMove.cs
@@ -7,6 +7,7 @@ public class AIMove : MonoBehaviour
     private Vector3 rallyPoint;  //this is the bridge they defend. In later builds, this will be public, and different enemy bases will have their own rally points we'll set from Unity
     private bool inPursuit = false; //we keep track of this so that we know if we're in a guarding or more offensive state
     private GameObject pursuing; //this is the immediate adversary of the enemy unit
+    private float pursueDistance = 7;
 
     void Start()
     {
@@ -40,19 +41,52 @@ public class AIMove : MonoBehaviour
             }
         }
         //if not in pursuit
-        GameObject[] units = GameObject.FindGameObjectsWithTag("Player"); //find every player unit    
+        GameObject[] units = GameObject.FindGameObjectsWithTag("Player"); //find every player unit   
+        float shortestDistance = Mathf.Infinity;
+        GameObject closestUnit = null;
       
         foreach (GameObject g in units) //for all player units
         {
             distanceToPlayerUnit = Vector3.Distance(g.transform.position, transform.position);
 
-            if (distanceToPlayerUnit < 7) //if it's near a player unit, get into pursuit mode and chase them
+            if (distanceToPlayerUnit < pursueDistance && distanceToPlayerUnit < shortestDistance)
+                closestUnit = g;
+
+        }
+
+        if (closestUnit != null) //if there's something within range, setup pursuit
+        {
+
+            inPursuit = true;
+            pursuing = closestUnit;
+            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = pursuing.transform.position;
+            return;
+        }
+
+        //if no player units are nearby, see if we're in a group
+
+        units = GameObject.FindGameObjectsWithTag("Enemy");
+        float distanceToOtherUnit;
+        int nearbyCt = 0;
+
+        foreach (GameObject g in units)
+        {
+            if (g.GetComponent<EnemyHarvester>()) //harvester doesn't count
+                continue;
+
+            distanceToOtherUnit = Vector3.Distance(g.transform.position, transform.position);
+
+            if (distanceToOtherUnit < 7)
             {
-                inPursuit = true;
-                pursuing = g;
-                GetComponent<UnityEngine.AI.NavMeshAgent>().destination = g.transform.position;
-                return;
+                nearbyCt++;
             }
+        }
+        //if we are in a group, rally at the player's base and aggressively pursue units
+        if (nearbyCt > 6)
+        {
+            rallyPoint = new Vector3(0f, 2f, 0f);
+            GetComponent<UnityEngine.AI.NavMeshAgent>().destination = rallyPoint;
+            pursueDistance = Mathf.Infinity;
         }
 
     }
