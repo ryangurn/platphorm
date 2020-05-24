b08fdf460c2c2809b3aaf36033f94566a5e0bf30
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-24 09:10:48 -0700

    Adding Tutorial Steps
    Adding steps for moving harvesters, pausing and unpausing. Currently working on tutorial to move the harvester specifically.

--- Assets/Scripts/UI/TutorialManager.cs
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -9,15 +9,20 @@ public class TutorialManager : MonoBehaviour
     public GameObject[] popups;
     private int popupIndex;
 
+    private GameObject[] units;
+    private GameObject[] harvesters;
+    private GameObject[] attackingUnits;
+
     // Start is called before the first frame update
     void Start()
     {
-
+      units = GameObject.FindGameObjectsWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+
       for (int i = 0; i < popups.Length; i++)
       {
         if (i == popupIndex)
@@ -60,6 +65,53 @@ public class TutorialManager : MonoBehaviour
           popupIndex++;
         }
       }
+      else if (popupIndex == 4)
+      {
+        if (Camera.GetComponent<CameraController>().isLocked)
+        {
+          popupIndex++;
+        }
+      }
+      else if (popupIndex == 5)
+      {
+        if (!Camera.GetComponent<CameraController>().isLocked)
+        {
+          popupIndex++;
+        }
+      }
+      else if (popupIndex == 6)
+      {
+        bool move = false;
+        foreach(GameObject unit in units)
+        {
+          // get the mesh renders
+          MeshRenderer[] unitChildren = unit.GetComponentsInChildren<MeshRenderer>();
+          foreach (MeshRenderer mr in unitChildren)
+      		{
+      			if (mr.gameObject.name == "SelectedSymbol")
+      			{
+      				// Make the selected symbol mesh visible or not
+      				if(mr.enabled)
+              {
+                move = true;
+              }
+      			}
+      		}
 
+          if(move)
+          {
+            popupIndex++;
+          }
+        }
+      }
+      else if (popupIndex == 7)
+      {
+        // do the same thing for the harvester
+
+        popupIndex++;
+      }
+
+
+      units = GameObject.FindGameObjectsWithTag("Player");
     }
 }
