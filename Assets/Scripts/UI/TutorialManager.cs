64691de7b5a6686c13076f332cb603046bd7c966
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-23 23:13:54 -0700

    Adding Tutorial for Camera Controls!
    This will allow the player to learn how things work!

---
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popups;
+    private int popupIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      for (int i = 0; i < popups.Length; i++)
+      {
+        if (i == popupIndex)
+        {
+          popups[i].SetActive(true);
+        }
+        else
+        {
+          popups[i].SetActive(false);
+        }
+      }
+
+
+      if (popupIndex == 0)
+      {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+          popupIndex++;
+        }
+
+      }
+      else if (popupIndex == 1)
+      {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+          popupIndex++;
+        }
+      }
+      else if (popupIndex == 2)
+      {
+        if ( Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+          popupIndex++;
+        }
+      }
+
+    }
+}
