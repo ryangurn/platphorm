e4e4b3635de8f5894a892d070355afec403e50e8
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-24 10:50:06 -0700

    Adding more instructions to the tutorial
    Adding a new navmesh specific to the tutorial since I dont want a full enemy platform in this scene, the player ends up getting destroyed too quickly if that is the case.
    
    Instead I will look at just spawning a few enemies when needed on a seperate platform.

--- Assets/Scripts/UI/TutorialManager.cs
+++ Assets/Scripts/UI/TutorialManager.cs
@@ -1,117 +1,188 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    public GameObject Camera;
+  public GameObject Camera;
 
-    public GameObject[] popups;
-    private int popupIndex;
+  public GameObject[] popups;
+  private int popupIndex;
+  private int changeCountHarvester;
+  private int changeCountAttack;
 
-    private GameObject[] units;
-    private GameObject[] harvesters;
-    private GameObject[] attackingUnits;
+  private GameObject[] units;
+  private List<GameObject> harvesters = new List<GameObject>();
+  private List<GameObject> attackingUnits = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
+  void UpdateUnits()
+  {
+    units = GameObject.FindGameObjectsWithTag("Player");
+    foreach (GameObject unit in units)
     {
-      units = GameObject.FindGameObjectsWithTag("Player");
+      if (unit.name == "Harvester") {
+        harvesters.Add(unit);
+      }
+      else
+      {
+        attackingUnits.Add(unit);
+      }
+    }
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    changeCountHarvester = 0;
+    changeCountAttack = 0;
+    UpdateUnits();
+  }
+
+  // Update is called once per frame
+  void LateUpdate()
+  {
+
+    for (int i = 0; i < popups.Length; i++)
+    {
+      if (i == popupIndex)
+      {
+        popups[i].SetActive(true);
+      }
+      else
+      {
+        popups[i].SetActive(false);
+      }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    if (popupIndex == 0)
     {
-
-      for (int i = 0; i < popups.Length; i++)
+      if (Camera.GetComponent<CameraController>().CameraFrontBack)
       {
-        if (i == popupIndex)
-        {
-          popups[i].SetActive(true);
-        }
-        else
-        {
-          popups[i].SetActive(false);
-        }
-      }
-
-
-      if (popupIndex == 0)
-      {
-        if (Camera.GetComponent<CameraController>().CameraFrontBack)
-        {
-          popupIndex++;
-        }
-
-      }
-      else if (popupIndex == 1)
-      {
-        if (Camera.GetComponent<CameraController>().CameraLeftRight)
-        {
-          popupIndex++;
-        }
-      }
-      else if (popupIndex == 2)
-      {
-        if ( Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetAxis("Mouse ScrollWheel") < 0f)
-        {
-          popupIndex++;
-        }
-      }
-      else if (popupIndex == 3)
-      {
-        if (Camera.GetComponent<CameraController>().isPanning)
-        {
-          popupIndex++;
-        }
-      }
-      else if (popupIndex == 4)
-      {
-        if (Camera.GetComponent<CameraController>().isLocked)
-        {
-          popupIndex++;
-        }
-      }
-      else if (popupIndex == 5)
-      {
-        if (!Camera.GetComponent<CameraController>().isLocked)
-        {
-          popupIndex++;
-        }
-      }
-      else if (popupIndex == 6)
-      {
-        bool move = false;
-        foreach(GameObject unit in units)
-        {
-          // get the mesh renders
-          MeshRenderer[] unitChildren = unit.GetComponentsInChildren<MeshRenderer>();
-          foreach (MeshRenderer mr in unitChildren)
-      		{
-      			if (mr.gameObject.name == "SelectedSymbol")
-      			{
-      				// Make the selected symbol mesh visible or not
-      				if(mr.enabled)
-              {
-                move = true;
-              }
-      			}
-      		}
-
-          if(move)
-          {
-            popupIndex++;
-          }
-        }
-      }
-      else if (popupIndex == 7)
-      {
-        // do the same thing for the harvester
-
         popupIndex++;
       }
 
-
-      units = GameObject.FindGameObjectsWithTag("Player");
     }
+    else if (popupIndex == 1)
+    {
+      if (Camera.GetComponent<CameraController>().CameraLeftRight)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 2)
+    {
+      if ( Input.GetAxis("Mouse ScrollWheel") > 0f || Input.GetAxis("Mouse ScrollWheel") < 0f)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 3)
+    {
+      if (Camera.GetComponent<CameraController>().isPanning)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 4)
+    {
+      if (Camera.GetComponent<CameraController>().isLocked)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 5)
+    {
+      if (!Camera.GetComponent<CameraController>().isLocked)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 6)
+    {
+      bool move = false;
+      foreach(GameObject unit in units)
+      {
+        // get the mesh renders
+        MeshRenderer[] unitChildren = unit.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer mr in unitChildren)
+        {
+          if (mr.gameObject.name == "SelectedSymbol")
+          {
+            // Make the selected symbol mesh visible or not
+            if(mr.enabled)
+            {
+              move = true;
+            }
+          }
+        }
+      }
+
+      if(move)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 7)
+    {
+      bool unselected = true;
+      foreach(GameObject unit in units)
+      {
+        // get the mesh renders
+        MeshRenderer[] unitChildren = unit.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer mr in unitChildren)
+        {
+          if (mr.gameObject.name == "SelectedSymbol")
+          {
+
+            if(mr.enabled)
+            {
+              unselected = false;
+            }
+          }
+        }
+      }
+
+      if(unselected)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 8)
+    {
+      // do the same thing for the harvester
+      foreach (GameObject harvest in harvesters) {
+        // check if there is motion
+        if (harvest.transform.hasChanged)
+        {
+          changeCountHarvester++;
+          harvest.transform.hasChanged = false;
+        }
+      }
+
+      if (changeCountHarvester >= 100)
+      {
+        popupIndex++;
+      }
+    }
+    else if (popupIndex == 9)
+    {
+      // do the same thing for the harvester
+      foreach (GameObject attack in attackingUnits) {
+        // check if there is motion
+        if (attack.transform.hasChanged)
+        {
+          changeCountAttack++;
+          attack.transform.hasChanged = false;
+        }
+      }
+
+      if (changeCountAttack >= 100)
+      {
+        popupIndex++;
+      }
+    }
+
+    UpdateUnits();
+  }
 }
