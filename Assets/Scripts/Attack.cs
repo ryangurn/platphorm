caa4258475bba5c47805c82a3760ae6d9a0fec3e
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-25 20:28:32 -0700

    Working on adding functionality to tech center
    Adding the ability to add upgrades to units that currently exist in the world. This will not currently edit the stats on newly spawned objects but we will get to that.

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     public string MyEnemy; //this component is shared, so we need to specify who were're trying to attack and damage
- 
+    public float HealthDecrement = 0.015f;
+    public int AttackRange = 3;
+
     void FixedUpdate()
     {
         GameObject[] units = GameObject.FindGameObjectsWithTag(MyEnemy); //look for all enemy units
 
         foreach (GameObject g in units)
         {
-            if (Vector3.Distance(g.transform.position, transform.position) < 3) //if they're nearby, reduce their health and run their damage "animation"
+            if (Vector3.Distance(g.transform.position, transform.position) < AttackRange) //if they're nearby, reduce their health and run their damage "animation"
             {
-                g.GetComponent<Health>().HealthLevel -= .015f;
+                g.GetComponent<Health>().HealthLevel -= HealthDecrement;
                 g.GetComponent<Health>().RunDamageAnimation();
             }
         }
---
+++ Assets/Scripts/TechCenterController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TechCenterController : MonoBehaviour
+{
+
+  public GameObject PlayerResources;
+
+  public int RangeDelta = 1;
+  public int RangeLimit = 2;
+  public int RangePerCost = 300;
+
+  public float SpeedDelta = 1.5f;
+  public int SpeedLimit = 2;
+  public int SpeedPerCost = 300;
+
+  public float StrengthDelta = 2.0f;
+  public int StrengthLimit = 2;
+  public int StrengthPerCost = 800;
+
+  private int RangeCounter = 0;
+  private int SpeedCounter = 0;
+  private int StrengthCounter = 0;
+
+
+  private GameObject[] units;
+  private List<GameObject> harvesters = new List<GameObject>();
+  private List<GameObject> attackUnits = new List<GameObject>();
+
+  void Start()
+  {
+
+    units = GameObject.FindGameObjectsWithTag("Player");
+    foreach (GameObject unit in units)
+    {
+      if (unit.name == "Harvester") {
+        harvesters.Add(unit);
+      }
+      else
+      {
+        attackUnits.Add(unit);
+      }
+
+    }
+  }
+
+  public void ChangeRangeOfAttack()
+  {
+    if (RangeCounter == RangeLimit) return;
+
+    if (PlayerResources.GetComponent<SupplyInventory>().Supplies < RangePerCost) return;
+
+    PlayerResources.GetComponent<SupplyInventory>().Supplies -= RangePerCost;
+
+    foreach (GameObject attack in attackUnits)
+    {
+      attack.GetComponent<Attack>().AttackRange = attack.GetComponent<Attack>().AttackRange + RangeDelta;
+    }
+    return;
+  }
+
+  public void ChangeSpeedOfUnits()
+  {
+    if (SpeedCounter == SpeedLimit) return;
+
+    if (PlayerResources.GetComponent<SupplyInventory>().Supplies < SpeedPerCost) return;
+
+    PlayerResources.GetComponent<SupplyInventory>().Supplies -= SpeedPerCost;
+
+    foreach (GameObject unit in units)
+    {
+      unit.GetComponent<NavMeshAgent>().speed = unit.GetComponent<NavMeshAgent>().speed + SpeedDelta;
+    }
+    return;
+  }
+
+  public void ChangeStrengthOfUnits()
+  {
+    if (StrengthCounter == StrengthLimit) return;
+
+    if (PlayerResources.GetComponent<SupplyInventory>().Supplies < StrengthPerCost) return;
+
+    PlayerResources.GetComponent<SupplyInventory>().Supplies -= StrengthPerCost;
+
+    foreach (GameObject unit in units)
+    {
+      unit.GetComponent<Health>().HealthLevel = unit.GetComponent<Health>().HealthLevel + StrengthDelta;
+    }
+    return;
+  }
+
+  public void AddSlotToHarvester()
+  {
+    // todo
+  }
+}
