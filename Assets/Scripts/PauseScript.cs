09cffa8e3d1b60018d23c26a375aa919d0d3f69b
Thomas Mitchell <tfm@uoregon.edu>
2020-06-01 21:42:49 -0700

    Added Pause Script
    I added a script for pausing and relevant entries changes to the menu and exit button objects in the scene. Player starting cash has been set higher temporarily for troubleshooting purposes.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (gameObject.activeInHierarchy)
+            Pause();
+
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void unPause()
+    {
+        Time.timeScale = 1;
+    }
+}
